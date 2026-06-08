f3f418cb57fb83e1c91e1d26f54af60fd547c665
nmd26 <diopmaguette1978@gmail.com>
2026-06-08 19:09:16 +0100

    consul

--- src/main/java/ma/osbt/config/KeepAliveScheduler.java
+++ src/main/java/ma/osbt/config/KeepAliveScheduler.java
@@ -1,5 +1,19 @@
 package ma.osbt.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
 public class KeepAliveScheduler {
 
+    @Autowired
+    private SimpMessagingTemplate messagingTemplate;
+
+    // 🔥 toutes les 25 secondes
+    @Scheduled(fixedRate = 25000)
+    public void sendKeepAlive() {
+        messagingTemplate.convertAndSend("/topic/ping", "ping");
+    }
 }
